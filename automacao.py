dcd29b46bef60c53d503cdb15f7f47c4d4fc8350
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-10-03 18:05:33 -0300

    iniciando automação

---
+++ automacao.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.service import Service
+import time
+
+# automatizar romaneios e nro de carga com 
+# notepad
+romaneios = []
+service = Service(ChromeDriverManager().install())
+
+browser = webdriver.Chrome(service=service)
+browser.get('https://gfl.sinclog.com.br/EntregasRelatorios/RelatorioGeralEntregas')
+
+
+# login
+browser.find_element('xpath', '//*[@id="login"]').send_keys('gustavo.varadi1')
+browser.find_element('xpath', '//*[@id="senha"]').send_keys('Lokalti3601!!!')
+browser.find_element('xpath', '//*[@id="formLogin"]/button').click()
+
+# relatorio v2
+
+# romaneio
+time.sleep(4)
+browser.find_element('xpath', 'form-control input-sm').click()
+romaneio = browser.find_element('xpath', 'option[value=nro_lista]')
+time.sleep(0.5)
+# browser.find_element('xpath', '//*[@id="tipoBusca"]/option[11]').click()
+# time.sleep(0.25)
+select_romaneio = browser.find_element('xpath', 'tipoBusca')
+select = Select(select_romaneio)
+select.select_by_value('nro_lista')
+
+
---
+++ teste.py
@@ -0,0 +1,7 @@
+romaneios = [123,456,789]
+
+resultado = (i for i in romaneios)
+resultado
+
+# for i in romaneios:
+#     print(i)
