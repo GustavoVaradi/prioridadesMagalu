9d4071dbacc6902d0cfdde12de17df8e86828f94
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-10-10 17:20:33 -0300

    zona de testes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,31 @@
-# encoding='ISO-8859-1', index_col=False
-
 import pandas as pd
 import streamlit as st
+from datetime import datetime
 #import plotly.express as px
-
-
-
-st.set_page_config(layout="wide")
-#decimal=","
-df = pd.read_csv("prioridades.csv", encoding="ISO-8859-1",
-                  index_col=False, sep=";")
-
-df["Nro. Entrega"] = df["Nro. Entrega"].astype(object)
-df['Numero End. Pessoa Visita'] = df['Numero End. Pessoa Visita'].astype(object)
-
-# Nro. Entrega | Sigla Unidade Atual | 
-# (Logradouro Pessoa Visita + Numero End. Pessoa Visita) |
-# Valor Total | Status 
 
 def transform_string(s):
     changed = s.replace('R$', '').replace('.','').replace(',','.')
     return float(changed)
 
-df['Valor Total'] = df['Valor Total'].apply(transform_string)
+st.set_page_config(layout="wide")
+data_hoje = datetime.day
 
+df = pd.read_csv("prioridades 10.10.csv", index_col=False, sep=",")
+
+df["Nro. Entrega"] = df["Nro. Entrega"].astype(object)
+df = df.drop_duplicates(subset=['Nro. Entrega'])
+df['Numero End. Pessoa Visita'] = df['Numero End. Pessoa Visita'].astype(object)
+df['Valor Total'] = df['Valor Total'].apply(transform_string)
 endereco = df['Logradouro Pessoa Visita'] + ' - nº ' +df['Numero End. Pessoa Visita'].astype(str)
+
+unidades_atuais = ['GRU', 'VGI', 'ABC', 'OSA']
+df = df[df['Sigla Unidade Atual'].isin(unidades_atuais)]
+
+status_filtro = ['ENTREGUE', 'EM ROTA', 'PENDENTE']
+df.loc[~df['Status'].isin(status_filtro), 'Status'] = 'NÃO ROTEIRIZADO'
+
+# (Logradouro Pessoa Visita + Numero End. Pessoa Visita)
+# Valor Total | Status  | Nro. Entrega | Sigla Unidade Atual |
 
 data = {'Entrega': df["Nro. Entrega"],
         'Unidade': df["Sigla Unidade Atual"],
@@ -38,8 +39,6 @@ data = {'Entrega': df["Nro. Entrega"],
 df_novo = pd.DataFrame(data)
 
 df_novo = df_novo[(df_novo['Valor Total']>=3000)]
-# df_novo['Status'] = df_novo['Status'].loc[df_novo['Status'] != ['EM ROTA', ' ENTREGUE'], 'Status'] = 'NÃO ROTEIRIZADO'
-# df_novo['Status'].unique()
 
 df_novo = df_novo.sort_values(["Valor Total"], ascending=False)
 
@@ -50,8 +49,10 @@ df_novo['Entrega'] = df_novo['Entrega'].astype(str)
 status = st.sidebar.selectbox("Status", df_novo['Status'].unique())
 unidade = st.sidebar.selectbox("Base", df_novo['Unidade'].unique())
 
-unidade_filtro = ['GRU', 'VGI', 'ABC', 'OSA']
-status_filtro = ['ENTREGUE', 'EM ROTA', 'NÃO ROTEIRIZADO']
+
+# for i in unidade_filtro:
+#     if df_novo['Unidade'].item() == i:
+#         df_novo = df_novo[df_novo['Unidade']== i]
 
 df_novo = df_novo[df_novo['Status']== status]
 df_novo = df_novo[df_novo['Unidade'] == unidade]
