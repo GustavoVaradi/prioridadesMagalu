63bdbb8aa953f84642cbb508d7c4126ce3cac82c
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-10-11 08:44:18 -0300

    melhorando o desempenho com low memory false

--- uniao_romaneios.py
+++ uniao_romaneios.py
@@ -4,14 +4,10 @@ data_hoje = datetime.today().strftime('%d.%m')
 print(data_hoje)
 
 
-df_abc = pd.read_csv(f'romaneios/romaneios abc {data_hoje}.csv', sep=';', 
-                                    encoding="ISO-8859-1", index_col=False)
-df_gru = pd.read_csv(f'romaneios/romaneios gru {data_hoje}.csv', sep=';', 
-                                    encoding="ISO-8859-1", index_col=False)
-df_osa= pd.read_csv(f'romaneios/romaneios osa {data_hoje}.csv', sep=';',  
-                                    encoding="ISO-8859-1", index_col=False)
-df_vgi= pd.read_csv(f'romaneios/romaneios vgi {data_hoje}.csv', sep=';',  
-                                    encoding="ISO-8859-1", index_col=False)
+df_abc = pd.read_csv(f'romaneios/romaneios abc {data_hoje}.csv', low_memory=False, sep=';', encoding="ISO-8859-1", index_col=False)
+df_gru = pd.read_csv(f'romaneios/romaneios gru {data_hoje}.csv', low_memory=False, sep=';', encoding="ISO-8859-1", index_col=False)
+df_osa= pd.read_csv(f'romaneios/romaneios osa {data_hoje}.csv', low_memory=False, sep=';',  encoding="ISO-8859-1", index_col=False)
+df_vgi= pd.read_csv(f'romaneios/romaneios vgi {data_hoje}.csv', low_memory=False, sep=';',  encoding="ISO-8859-1", index_col=False)
 
 df = pd.concat([df_vgi, df_abc, df_gru, df_osa], ignore_index=True)
 
