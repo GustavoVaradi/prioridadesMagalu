034b39d7a9d7cba6e988c5aa17e33562b05d93de
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-10-16 10:20:28 -0300

    iha

--- uniao_romaneios.py
+++ uniao_romaneios.py
@@ -7,9 +7,11 @@ df_abc = pd.read_csv(f'{caminho}/romaneios abc {data_hoje}.csv', low_memory=Fals
 df_gru = pd.read_csv(f'{caminho}/romaneios gru {data_hoje}.csv', low_memory=False, sep=';', encoding="ISO-8859-1", index_col=False)
 df_osa= pd.read_csv(f'{caminho}/romaneios osa {data_hoje}.csv', low_memory=False, sep=';',  encoding="ISO-8859-1", index_col=False)
 df_vgi= pd.read_csv(f'{caminho}/romaneios vgi {data_hoje}.csv', low_memory=False, sep=';',  encoding="ISO-8859-1", index_col=False)
+df_iha = pd.read_csv(f'{caminho}/iha.csv', low_memory=False, sep=';',  encoding="ISO-8859-1", index_col=False)
 
 df_concat = pd.concat([df_vgi, df_osa, df_abc, df_gru], ignore_index=True)
-df_concat.to_csv(f'C:/Users/Varad/Programming/Magalu/arcadeteste/romaneios/romaneios geral.csv', index=False)
+#df_concat.to_csv(f'C:/Users/Varad/Programming/Magalu/arcadeteste/romaneios/romaneios geral.csv', index=False)
+df_iha.to_csv(f'C:/Users/Varad/Programming/Magalu/arcadeteste/romaneios/romaneios geral.csv', index=False)
 
 df = pd.read_csv(f'{caminho}/romaneios geral.csv',low_memory=False, sep=',', index_col=False)
 df.to_csv(f'C:/Users/Varad/Programming/Magalu/arcadeteste/prioridades {data_hoje}.csv',
